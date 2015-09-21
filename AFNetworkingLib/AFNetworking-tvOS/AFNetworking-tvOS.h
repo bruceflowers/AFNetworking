899fc0853fbf1ebcb805c2da9dd93f528c8a1df7
bruceflowers <flowers.bruce@gmail.com>
2015-09-21 15:23:36 -0400

    adding tvOS support

---
+++ AFNetworkingLib/AFNetworking-tvOS/AFNetworking-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  AFNetworking-tvOS.h
+//  AFNetworking-tvOS
+//
+//  Created by Bruce Flowers on 9/21/15.
+//  Copyright © 2015 Alamofire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFNetworking-tvOS.
+FOUNDATION_EXPORT double AFNetworking_tvOSVersionNumber;
+
+//! Project version string for AFNetworking-tvOS.
+FOUNDATION_EXPORT const unsigned char AFNetworking_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFNetworking_tvOS/PublicHeader.h>
+
+
